9594ac109bceb3a434ef6c06e539f4bc7343f704
Oleg Kuziv <33831834+olkuziv@users.noreply.github.com>
2017-11-24 21:16:18 +0200

    Створена проста архітектура проекту

---
+++ VKStatistic/Classes/Friend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKStatistic.Classes
+{
+    class Friend : IUser
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+
+        public int Count(SearchStatus status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VKStatistic/Classes/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKStatistic.Classes
+{
+    class User : IUser
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public Friend[] GetLastVisited() {
+            throw new NotImplementedException();
+        }
+        public Friend[] GetLastVisited(int count) {
+            throw new NotImplementedException();
+        }
+
+        public int Count(SearchStatus status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
