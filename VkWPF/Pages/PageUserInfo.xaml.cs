28b8a8c9a6261be65eef16270e51554533c096c8
Oleg Kuziv <33831834+olkuziv@users.noreply.github.com>
2017-12-07 21:22:31 +0200

    AddedPages

--- VkWPF/Pages/PageUserInfo.xaml.cs
+++ VkWPF/Pages/PageUserInfo.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MahApps.Metro.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,39 +10,38 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using VkNet;
 using VkNet.Enums.Filters;
 
-namespace VkWPF
+namespace VkWPF.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для Window1.xaml
+    /// Логика взаимодействия для PageUserInfo.xaml
     /// </summary>
-    public partial class Window1
+    public partial class PageUserInfo : Page
     {
-        public Window1()
+        public PageUserInfo()
         {
             InitializeComponent();
-
             ulong appID = 6284662;                  // ID приложения
             string email = "reskot@mail.ua";        // email или телефон
-            string pass  = "ok2504762936OK";         // пароль для авторизации
+            string pass = "ok2504762936OK";         // пароль для авторизации
             Settings scope = Settings.All;          // Приложение имеет доступ к друзьям
 
             var vk = new VkApi();
             vk.Authorize(new ApiAuthParams
-                {
-                    ApplicationId = appID,
-                    Login = email,
-                    Password = pass,
-                    Settings = scope
-                }
+            {
+                ApplicationId = appID,
+                Login = email,
+                Password = pass,
+                Settings = scope
+            }
             );
             var info = vk.Account.GetProfileInfo();
             tFName.Text = info.FirstName;
             tLName.Text = info.LastName;
-
         }
     }
 }
---
+++ VkWPF/WindowStart.xaml.cs
@@ -0,0 +1,40 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using VkNet;
+using VkNet.Enums.Filters;
+
+namespace VkWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class WindowStart
+    {
+        public WindowStart()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUserInfo_Click(object sender, RoutedEventArgs e)
+        {
+            frameMain.Content = new Pages.PageUserInfo();
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            frameMain.Content = new Pages.PageSettings();
+        }
+    }
+}
