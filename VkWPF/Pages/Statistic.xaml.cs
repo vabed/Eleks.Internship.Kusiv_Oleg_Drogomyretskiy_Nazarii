3fa02d871f23fb563fc64896907ebbbd7e2cedd7
Oleg Kuziv <33831834+olkuziv@users.noreply.github.com>
2017-12-20 13:42:26 +0200

    1

--- VkWPF/Pages/Statistic.xaml.cs
+++ VkWPF/Pages/Statistic.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -15,14 +16,21 @@ using System.Windows.Shapes;
 
 namespace VkWPF.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Statistic.xaml
-    /// </summary>
+   
     public partial class Statistic : Page
     {
+        Dictionary<int, double> value;
         public Statistic()
         {
             InitializeComponent();
+            value = new Dictionary<int, double>();
+            for (int i = 1; i <= 2; i++)
+                value.Add(i, 10 * i);
+
+            Chart chart = this.FindName("MyWinformChart") as Chart;
+            chart.DataSource = value;
+            chart.Series["series"].XValueMember = "Key";
+            chart.Series["series"].YValueMembers = "Value";
         }
     }
 }
